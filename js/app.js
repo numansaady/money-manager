e5b061b83a5aa96119b3f0c37d574833b0dd6bbe
Numan Saady <numansaady@gmail.com>
2022-02-17 04:37:59 +0600

    Get input value with a function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Get input value
+function getInputValue(item) {
+  const inputValue = document.getElementById(item + "-value");
+  const amount = parseInt(inputValue.value);
+  return amount;
+}
+
+document
+  .getElementById("calculate-btn")
+  .addEventListener("click", function () {});
